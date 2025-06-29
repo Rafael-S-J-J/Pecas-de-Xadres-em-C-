74d0456e1647c796591c1eeac9ebda0bb9b77c8e
Rafael Silva <juniornill123@gmail.com>
2025-06-29 19:10:54 -0300

    Criando um menu para interação e preparando o escopo do switch

---
+++ xadrez.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+    int option;
+
+    printf("1 - Inciar jogo\n");
+    printf("2 - Como funicona ?\n");
+    printf("3 - Sair...\n");
+    scanf("%d\n", &option);
+
+    switch (option)
+    {
+    
+        case 1:
+        /* code */
+        break;
+
+        case 2:
+
+        case 3:
+    
+        default:
+        break;
+    }
+
+
+    
+    
+    
+    
+    
+    
+    return 0;
+}
